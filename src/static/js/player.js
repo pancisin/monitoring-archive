0de448f90783758819ad253a42bb7aa1c89cac39
Patrik Pancisin <patrik.pancisin.extern@rwe.com>
2025-10-19 12:03:56 +0200

    feat: add frame-by-frane navigation to timelapse playback

--- src/static/js/player.js
+++ src/static/js/player.js
@@ -1,6 +1,35 @@
+const createControl =  (label, handler) => {
+    const button = document.createElement('button');
+    button.className = 'plyr__control';
+    button.innerHTML = label;
+    button.title = label;
+    button.onclick = handler;
+    button.class = "user-select-none"
+
+    return button;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+    const fps = 10;
     const player = new Plyr('#player', {
           hideControls: false,
           fullscreen: { enabled: true, fallback: true },
+           controls: [
+            'play', 'progress', 'current-time', 'duration', 'settings', 'fullscreen', 'prevframe', 'nextframe'
+          ]
     });
+
+    const controls = document.querySelector('.plyr__controls');
+
+    const prevBtn = createControl("<", () => {
+        player.pause();
+        player.currentTime -= 1 / fps;
+    })
+    controls.appendChild(prevBtn);
+
+    const nextBtn = createControl(">", () => {
+        player.pause();
+        player.currentTime += 1 / fps;
+    });
+    controls.appendChild(nextBtn);
 });
